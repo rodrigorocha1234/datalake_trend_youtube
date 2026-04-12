0a157f36acce49be7bd5bce095604961d75ed8f2
rodrigorocha1234 <silva.rodrigo31@gmail.com>
2026-04-12 12:45:55 -0300

    feat: adição do minio como protocolo de conexão

--- src/operacao_banco/config/protocols.py
+++ src/operacao_banco/config/protocols.py
@@ -1,7 +1,14 @@
-from typing import Protocol, Any
+from typing import Any, Protocol
+
 import mssql_python
+from minio import Minio
 
 
 class MSSQLConnect(Protocol):
     def __call__(self, *args: Any, **kwargs: Any) -> mssql_python.Connection:
+        ...
+
+
+class MinioConnect(Protocol):
+    def __call__(self, *args: Any, **kwargs: Any) -> Minio:
         ...
