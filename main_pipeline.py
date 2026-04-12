6d12080eb97e4073f5d701ebc65885f8a2748149
rodrigorocha1234 <silva.rodrigo31@gmail.com>
2026-04-12 15:51:33 -0300

    refactor: migrate S3 integration to MinIO, update logging architecture, and switch to Jedi language server

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,22 +1,24 @@
-from src.contexto.contexto import Contexto
+
 from src.servicos.api_youtube.api_youtube import YoutubeAPI
-from src.servicos.servico_s3.s3_base import S3Base
-from src.corrente.checar_conexao_corrente import ChecarConexaCorrente
 from src.operacao_banco.config.db_config_sqlserverlog import \
     DbConfigSQLServerLOG
-from src.operacao_banco.operacao.operacao_sql_server import \
-    OperacaoSqlServerLOG
 
-from src.utils.log_banco import LogBanco
+from src.servicos.banco_analitico.servico_s3.db_config_minio import \
+    ConfigS3Minio
+from src.servicos.banco_analitico.servico_s3.operacao_minio_s3 import \
+    OperacaoMInioS3
+
+from src.utils.log_sql_server import LogBancoSQLServer
 
 db_config_log = DbConfigSQLServerLOG()
 
-db_operacao_log = OperacaoSqlServerLOG(conexao=db_config_log)
-conexao_s3 = S3Base()
+
+configuracao_s3 = ConfigS3Minio()
+operacao_s3 = OperacaoMInioS3(conexao_s3=configuracao_s3)
 
 
-config_log = LogBanco(
-    db_operacao=db_operacao_log,
+config_log = LogBancoSQLServer(
+    configuracao_conexao=db_config_log,
     debug="INFO",
     formato_log="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     nome_pacote="main_pipeline",
@@ -24,14 +26,13 @@ config_log = LogBanco(
 
 
 api_youtube = YoutubeAPI(conexao_log=config_log)
-contexto = Contexto()
 
 
-p1 = ChecarConexaCorrente(
-    conexao_log=config_log,
-    conexao_s3=conexao_s3,
-    conexao_youtube=api_youtube,
-)
+# p1 = ChecarConexaCorrente(
+#     conexao_log=config_log,
+#     conexao_s3=conexao_s3,
+#     conexao_youtube=api_youtube,
+# )
 
 
-p1.corrente(contexto=contexto)
+# p1.corrente(contexto=contexto)
