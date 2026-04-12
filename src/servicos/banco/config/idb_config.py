5302f4790510a19c400063ec77747c6d48d4258e
rodrigorocha1234 <silva.rodrigo31@gmail.com>
2026-04-12 19:52:15 -0300

    feat: adição de tupla e dict nos imports

--- src/servicos/banco/config/idb_config.py
+++ src/servicos/banco/config/idb_config.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Tuple, Dict, Any
 
 TConn = TypeVar("TConn")
 TDriver = TypeVar("TDriver")
